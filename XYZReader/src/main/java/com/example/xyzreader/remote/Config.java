d4823ae8ff1edb1e917eb97d4c2417d38a30d482
johnrobinson <john@cjlws.co.uk>
2017-05-29 13:55:47 +0100

    New URL for JSON

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -1,19 +1,24 @@
 package com.example.xyzreader.remote;
 
+import android.util.Log;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Config {
     public static final URL BASE_URL;
+    private static final String TAG = "CONFIG";
 
     static {
         URL url = null;
         try {
           //  url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
             // Forum fix:
-            url = new URL("https://nspf.github.io/XYZReader/data.json");
+           // url = new URL("https://nspf.github.io/XYZReader/data.json");
+            url = new URL("https://go.udacity.com/xyz-reader-json" );
         } catch (MalformedURLException ignored) {
-            // TODO: throw a real error
+            Log.e(TAG, "Issue with internet connection - please check");
+            //TODO add user-visible error message
         }
 
         BASE_URL = url;
